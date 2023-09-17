b483558a6f140eff2cb6b790d8a6d61d88f3dd6a
Jack <Taylor@psych.uni-frankfurt.de>
2023-09-17 15:47:33 +0200

    set up IRkernel in the install.R file

--- install.R
+++ install.R
@@ -1,6 +1,10 @@
+# packages
 install.packages("IRkernel")
 install.packages("tibble")
 install.packages("dplyr")
 install.packages("Cairo")
 install.packages("ggplot2")
 install.packages("lme4")
+
+# set up IRkernel
+IRkernel::installspec()
