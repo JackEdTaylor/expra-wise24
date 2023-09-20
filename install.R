27b536b252817a4ece088a1f228522805da19a3d
Jack <Taylor@psych.uni-frankfurt.de>
2023-09-20 15:14:27 +0200

    set up graphics in the install.R file for interactive sessions

--- install.R
+++ install.R
@@ -8,3 +8,16 @@ install.packages("lme4")
 
 # set up IRkernel
 IRkernel::installspec()
+
+# set up Cairo graphics
+opt <- c(
+  "",
+  "options(repr.plot.width=3.5, repr.plot.height=3)",
+  "",
+  ".First <- function() {",
+  "  require(Cairo)",
+  "  CairoPNG()",
+  "}"
+)
+profile_loc <- file.path(R.home(component="etc"), "Rprofile.site")
+writeLines(opt, profile_loc)
