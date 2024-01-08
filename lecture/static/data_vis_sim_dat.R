1cec6ec8452ed2ac3133f8e9e0c00c8e722a1898
Jack <Taylor@psych.uni-frankfurt.de>
2024-01-08 11:22:24 +0100

    update data

--- lecture/static/data_vis_sim_dat.R
+++ lecture/static/data_vis_sim_dat.R
@@ -10,13 +10,13 @@ d <- tibble(
   time = rep(c("morning", "midday", "evening"), each=n_subj),
   time_morn_dum = ifelse(time == "morning", 1, 0),
   time_eve_dum = ifelse(time == "evening", 1, 0),
-  read_score = 100 + time_morn_dum * -23 + time_eve_dum * -13 + rnorm(n_subj, 0, 10)
+  read_score = 100 + time_morn_dum * -23 + time_eve_dum * -6 + rnorm(n_subj, 0, 10)
 ) |>
   mutate(
     memory_score = (read_score - 100) * case_when(
       time == "morning" ~ 1.03,
       time == "midday" ~ 0,
-      time == "evening" ~ 0.43
+      time == "evening" ~ 0.24
     ) + rnorm(n_subj, 0, 10) + 100
   ) |>
   select(-ends_with("dum")) |>
