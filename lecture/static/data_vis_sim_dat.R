b8d25e64f9c0b3fd081a1dc75e3fe3c72270e12e
Jack <Taylor@psych.uni-frankfurt.de>
2024-01-08 11:46:25 +0100

    add subject intercepts

--- lecture/static/data_vis_sim_dat.R
+++ lecture/static/data_vis_sim_dat.R
@@ -5,14 +5,20 @@ set.seed(1)
 
 n_subj <- 67
 
+subj_intercepts <- tibble(
+  subj_id = sprintf("s%02d", 1:n_subj),
+  subj_intercept = rnorm(n_subj, 0, 5)
+)
+
 d <- tibble(
   subj_id = rep(sprintf("s%02d", 1:n_subj), 3),
   time = rep(c("morning", "midday", "evening"), each=n_subj),
   time_morn_dum = ifelse(time == "morning", 1, 0),
   time_eve_dum = ifelse(time == "evening", 1, 0),
-  read_score = 100 + time_morn_dum * -23 + time_eve_dum * -6 + rnorm(n_subj, 0, 10)
 ) |>
+  left_join(subj_intercepts, by="subj_id") |>
   mutate(
+    read_score = 100 + time_morn_dum * -23 + time_eve_dum * -6 + subj_intercept + rnorm(n_subj, 0, 5),
     memory_score = (read_score - 100) * case_when(
       time == "morning" ~ 1.03,
       time == "midday" ~ 0,
